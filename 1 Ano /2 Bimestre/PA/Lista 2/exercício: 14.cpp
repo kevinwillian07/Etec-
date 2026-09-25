49f3064101ef7236250563a9aecc885b9954bf7d
Kevin Will <kevinwilliandasilvasouza@gmail.com>
2026-09-25 04:32:33 -0300

    Create exercício: 14.cpp

---
+++ 1 Ano /2 Bimestre/PA/Lista 2/exercício: 14.cpp
@@ -0,0 +1,34 @@
+/*EtecVAV 14/06/2026
+*Autor: Kevin Willian
+ler 10 nomes em um vetor, ordenar em ordem decrescente e exibir em ordem crescente*/
+
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main() {
+    string nomes[10];
+    string aux;
+
+    for (int i = 0; i < 10; i++) {
+        cout << "Digite o " << i + 1 << "o nome: ";
+        cin >> nomes[i];
+    }
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = i + 1; j < 10; j++) {
+            if (nomes[i] < nomes[j]) { 
+                aux = nomes[i];
+                nomes[i] = nomes[j];
+                nomes[j] = aux;
+            }
+        }
+    }
+
+    cout << "\nNomes em ordem crescente:\n";
+    for (int i = 9; i >= 0; i--) {
+        cout << nomes[i] << endl;
+    }
+
+    return 0;
+}
