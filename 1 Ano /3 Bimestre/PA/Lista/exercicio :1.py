365f87d2d1edb27097b37878db32743e42efad3e
Kevin Will <kevinwilliandasilvasouza@gmail.com>
2026-09-25 05:03:14 -0300

    Create exercicio :1.py

---
+++ 1 Ano /3 Bimestre/PA/Lista/exercicio :1.py
@@ -0,0 +1,12 @@
+    # Autor:Kevin William
+    # Data: Junho/2026
+    # Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
+
+número = int(input("Digite um número inteiro: "))
+
+if número > 0:
+    print("O número e positivo.")
+elif número < 0:
+    print("O número e negativo.")
+else:
+    print("O número e zero.")
